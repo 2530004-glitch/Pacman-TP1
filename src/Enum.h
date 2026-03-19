6fde6f267808c1fc96e1433f2fad0b88e842f2cd
Wilyam Boivin <2530004@collegebart.ca>
2026-03-19 11:37:37 -0400

    First modification to pacman game

---
+++ src/Enum.h
@@ -0,0 +1,15 @@
+enum class ObjectTag 
+{
+    PacMan,
+    Ghost,
+    Pellet
+};
+//Create a function to use the values for "ObjectTag"
+
+enum class GhostType
+{
+    Inky,
+    Pinky,
+    Clyde
+};
+//Create a function to use the values for "GhostType"
